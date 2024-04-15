b2c327e27a0eb2c8bac7c37900b4523b4b28887b
Jared Allen <ST10271869@vcconnect.edu.za>
2024-04-15 19:38:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.CompilerServices;
+
+/*Welcome to my program, this program was designed under the specifications to be able to take in a users input of a recipe. 
+ * The user will be prompted to enter recipe details like how many apples are needed etc. 
+ * Next the program is designed to be able to display the recipe once all parameters have been filled.
+ * The next step was to add scalling to the program, this allows the user to scale their recipe
+ * by either 0.5, 1.5, or by 2.0
+ * After this i added a remove function that will remove all the quantities of the recipe. 
+ * Lastly i added another remove function that allows for the user to completely remove the entirity of the recipe. 
+ * 
+ */
+
+namespace ProgPOE
+{
+    //__________________________________________Start Of File____________________________________________________\\
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+
+            while (true)
+            {
+                //Creating Menu options for the user that will allow them to do either of the following 
+                Console.WriteLine("Enter: 1 to Enter Recipe Details");
+                Console.WriteLine("Enter: 2 to Display The Recipe");
+                Console.WriteLine("Enter: 3 to Scale Recipe");
+                Console.WriteLine("Enter: 4 to Reset All Quantities");
+                Console.WriteLine("Enter: 5 to Clear The Recipe");
+                Console.WriteLine("Enter: 6 to Exit App");
+
+//______________________________________________________________________________________________________________________________\\
+
+                //Creating switch case for the menu items
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        recipe.EnterDetails();
+                        break;
+                    case "2":
+                        recipe.DisplayRecipe();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the Scalling Factor (0.5, 2, or 3): ");
+                        double factor = double.Parse(Console.ReadLine());
+                        recipe.ScaleRecipe(factor);
+                        break;
+                    case "4":
+                        recipe.ResetQuantity();
+                        break;
+                    case "5":
+                        recipe.ClearRecipe();
+                        break;
+                    case "6":
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+                }
+            }
+        }
+    }
+}
+/*
+ * BIBLIOGRAPHY
+ * https://stackoverflow.com/questions/5678216/all-possible-array-initialization-syntaxes
+ * https://www.geeksforgeeks.org/c-sharp-int32-struct/
+ * 
+ */ 
+
+//____________________________________________End Of File________________________________________________________\\
---
+++ Recipe.cs
@@ -0,0 +1,121 @@
+﻿using System;
+namespace ProgPOE { 
+
+//___________________________________________________Start Of File______________________________________________________________\\
+
+    class Recipe
+    {
+        //Creating array ans variables
+        private string[] ingrediants;
+        private string[] units;
+        private double[] quantity;
+        private string[] steps;
+
+//_______________________________________________________________________________________________________________________________\\
+        public Recipe()
+    {
+        //Adding the variables to the array 
+        ingrediants = new string[0];
+        units = new string[0];
+        quantity = new double[0];
+        steps = new string[0];
+    }
+
+        //_____________________________________________________________________________________________________________________________\\
+        public void EnterDetails()
+        {
+
+            // Prompting the user to enter the number of ingredients for the recipe
+            Console.WriteLine("Please Enter The Number of Ingredients: ");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            // Initializing arrays to store ingredient details
+            ingrediants = new string[numIngredients];
+            units = new string[numIngredients];
+            quantity = new double[numIngredients];
+
+            // Loop to input details for each ingredient
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Please Enter The Details For The Ingredient #{i + 1}: ");
+                Console.Write("Name Of Ingredient: ");
+                ingrediants[i] = Console.ReadLine();
+                Console.Write("Quantity: ");
+                quantity[i] = double.Parse(Console.ReadLine());
+                Console.Write("Unit Of Measurement: ");
+                units[i] = Console.ReadLine();
+            }
+
+            // Prompting the user to enter the number of steps for the recipe
+            Console.Write("Please Enter The Number Steps: ");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            // Initializing array to store recipe steps
+            steps = new string[numSteps];
+
+            // Loop to input details for each step
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.Write($"Please Enter Step#{i + 1}: ");
+                steps[i] = Console.ReadLine();
+            }
+        }
+
+
+        //_____________________________________________________________________________________________________________________________\\
+        //Method that displays the recipe
+        public void DisplayRecipe()
+    {
+        //Displaying the quantities and ingrediants
+        Console.WriteLine("Ingrediants");
+        for (int i = 0; i < ingrediants.Length; i++)
+        {
+            Console.WriteLine($"- {quantity[i]} {units[i]} of {ingrediants[i]}");
+        }
+
+        //Displaying the steps
+        Console.WriteLine("Steps:");
+        for (int i = 0; i < steps.Length; i++)
+        {
+            Console.WriteLine($"- {steps[i]}");
+
+        }
+    }
+
+//_____________________________________________________________________________________________________________________________\\
+    //Method to scale the recipe 
+    public void ScaleRecipe(double factor)
+    {
+        //Multiplying all the quantities by the scaling factor
+        for (int i = 0; i < quantity.Length; i++)
+        {
+            quantity[i] *= factor;
+        }
+    }
+
+//_____________________________________________________________________________________________________________________________\\
+    //Method to reste quantities to half
+    public void ResetQuantity()
+    {
+        //Resetting all the quantities to their original values
+        for (int i = 0; i < quantity.Length; i++)
+        {
+            quantity[i] /= 2;
+        }
+    }
+
+//_____________________________________________________________________________________________________________________________\\
+    //Method to clear recipe
+    public void ClearRecipe()
+    {
+        //Restting all the arrays to empty
+        ingrediants = new string[0];
+        quantity = new double[0];
+        units = new string[0];
+        steps = new string[0];
+       }
+    }
+}
+
+
+//____________________________________________________________End Of File_____________________________________________________\\
